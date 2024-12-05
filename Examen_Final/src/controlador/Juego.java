7cfd6092f43fccad1c5cdd1ff2500dc2c1c7cbba
Jostin Matamoros <alexander050601@gmail.com>
2024-12-05 17:04:52 -0500

    Reorganizacion de codigo

--- Examen_Final/src/controlador/Juego.java
+++ Examen_Final/src/controlador/Juego.java
@@ -5,47 +5,48 @@ import modelo.excepciones.ExcepcionJuego;
 import vista.Vista;
 
 public class Juego {
-    private final Tablero tablero;
-    private final Vista vista;
-    private boolean enJuego;
+	private final Tablero tablero;
+	private final Vista vista;
+	private boolean enJuego;
 
-    public Juego() {
-        // Crear el tablero y la vista
-        tablero = new Tablero(10, 10, 10); // Tablero 10x10 con 10 minas
-        vista = new Vista();
-        enJuego = true;
-    }
+	public Juego() {
+		// Crear el tablero y la vista
+		tablero = new Tablero(10, 10, 10); // Tablero 10x10 con 10 minas
+		vista = new Vista();
+		enJuego = true;
+	}
 
-    public void iniciar() {
-        tablero.inicializar();
+	public void iniciar() {
+		tablero.inicializar();
 
-        // Bucle principal del juego
-        while (enJuego) {
-            vista.mostrarTablero(tablero); 
+		// Bucle principal del juego
+		while (enJuego) {
+			vista.mostrarTablero(tablero);
 
-            String entrada = vista.solicitarEntrada("Introduce coordenadas (ejemplo: A5) o marca con 'M A5':");
+			String entrada = vista
+					.solicitarEntrada("Introduce coordenadas (ejemplo: A5) o marca la casilla con 'M A5':");
 
-            try {
-                if (entrada.startsWith("M")) {                   
-                    tablero.marcar(entrada.substring(2));
-                } else {
-                    tablero.descubrir(entrada);
+			try {
+				if (entrada.startsWith("M")) {
+					tablero.marcar(entrada.substring(2));
+				} else {
+					tablero.descubrir(entrada);
 
-                    if (tablero.victoria()) {
-                        vista.mostrarTablero(tablero);
-                        vista.mostrarMensaje("¡Felicidades, ganaste!");
-                        enJuego = false; 
-                    }
-                }
-            } catch (ExcepcionJuego e) {
-                vista.mostrarMensaje(e.getMessage()); 
-            }
-        }
+					if (tablero.victoria()) {
+						vista.mostrarTablero(tablero);
+						vista.mostrarMensaje("¡Felicidades, ganaste!");
+						enJuego = false;
+					}
+				}
+			} catch (ExcepcionJuego e) {
+				vista.mostrarMensaje(e.getMessage());
+			}
+		}
 
-        vista.mostrarMensaje("¡Gracias por jugar!");
-    }
+		vista.mostrarMensaje("¡Gracias por jugar!");
+	}
 
-    public static void main(String[] args) {
-        new Juego().iniciar();
-    }
+	public static void main(String[] args) {
+		new Juego().iniciar();
+	}
 }
--- Examen_Final/src/modelo/Tablero.java
+++ Examen_Final/src/modelo/Tablero.java
@@ -7,163 +7,167 @@ import modelo.excepciones.MinaDescubiertaException;
 
 import java.util.Random;
 
+//Atributos de la clase
 public class Tablero {
-    private final Casilla[][] casillas;
-    private final int filas;
-    private final int columnas;
-    private final int minas;
-    private int casillasSeguras;
+	private final Casilla[][] casillas;
+	private final int filas;
+	private final int columnas;
+	private final int minas;
+	private int casillasSeguras;
 
-    public Tablero(int filas, int columnas, int minas) {
-        this.filas = filas;
-        this.columnas = columnas;
-        this.minas = minas;
-        this.casillasSeguras = filas * columnas - minas;
-        casillas = new Casilla[filas][columnas];
-    }
+	// Constructor
+	public Tablero(int filas, int columnas, int minas) {
+		this.filas = filas;
+		this.columnas = columnas;
+		this.minas = minas;
+		this.casillasSeguras = filas * columnas - minas;
+		casillas = new Casilla[filas][columnas];
+	}
 
-    public void inicializar() {
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                casillas[i][j] = new Casilla();
-            }
-        }
-        colocarMinas();
-        calcularAdyacentes();
-    }
+	// Metodos
+	public void inicializar() {
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				casillas[i][j] = new Casilla();
+			}
+		}
+		colocarMinas();
+		calcularAdyacentes();
+	}
 
-    private void colocarMinas() {
-        Random random = new Random();
-        int colocadas = 0;
+	private void colocarMinas() {
+		Random random = new Random();
+		int colocadas = 0;
 
-        while (colocadas < minas) {
-            int fila = random.nextInt(filas);
-            int columna = random.nextInt(columnas);
+		while (colocadas < minas) {
+			int fila = random.nextInt(filas);
+			int columna = random.nextInt(columnas);
 
-            if (!casillas[fila][columna].esMina()) {
-                casillas[fila][columna].ponerMina();
-                colocadas++;
-            }
-        }
-    }
+			if (!casillas[fila][columna].esMina()) {
+				casillas[fila][columna].ponerMina();
+				colocadas++;
+			}
+		}
+	}
 
-    private void calcularAdyacentes() {
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                if (!casillas[i][j].esMina()) {
-                    casillas[i][j].setMinasAdyacentes(contarMinasAdyacentes(i, j));
-                }
-            }
-        }
-    }
+	private void calcularAdyacentes() {
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				if (!casillas[i][j].esMina()) {
+					casillas[i][j].setMinasAdyacentes(contarMinasAdyacentes(i, j));
+				}
+			}
+		}
+	}
 
-    private int contarMinasAdyacentes(int fila, int columna) {
-        int minas = 0;
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                int nuevaFila = fila + x;
-                int nuevaColumna = columna + y;
-                if (dentroLimites(nuevaFila, nuevaColumna) && casillas[nuevaFila][nuevaColumna].esMina()) {
-                    minas++;
-                }
-            }
-        }
-        return minas;
-    }
+	private int contarMinasAdyacentes(int fila, int columna) {
+		int minas = 0;
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				int nuevaFila = fila + x;
+				int nuevaColumna = columna + y;
+				if (dentroLimites(nuevaFila, nuevaColumna) && casillas[nuevaFila][nuevaColumna].esMina()) {
+					minas++;
+				}
+			}
+		}
+		return minas;
+	}
 
-    private boolean dentroLimites(int fila, int columna) {
-        return fila >= 0 && fila < filas && columna >= 0 && columna < columnas;
-    }
+	private boolean dentroLimites(int fila, int columna) {
+		return fila >= 0 && fila < filas && columna >= 0 && columna < columnas;
+	}
 
-    public void descubrir(String coordenada) throws ExcepcionJuego {
-        int[] indices = convertirCoordenada(coordenada);
-        int fila = indices[0];
-        int columna = indices[1];
+	public void descubrir(String coordenada) throws ExcepcionJuego {
+		int[] indices = convertirCoordenada(coordenada);
+		int fila = indices[0];
+		int columna = indices[1];
 
-        if (casillas[fila][columna].estaDescubierta()) {
-            throw new CasillaYaDescubiertaException();
-        }
+		if (casillas[fila][columna].estaDescubierta()) {
+			throw new CasillaYaDescubiertaException();
+		}
 
-        casillas[fila][columna].descubrir();
+		casillas[fila][columna].descubrir();
 
-        if (casillas[fila][columna].esMina()) {
-            throw new MinaDescubiertaException();
-        }
+		if (casillas[fila][columna].esMina()) {
+			throw new MinaDescubiertaException();
+		}
 
-        casillasSeguras--;
+		casillasSeguras--;
 
-        if (casillas[fila][columna].getMinasAdyacentes() == 0) {
-            revelarAdyacentes(fila, columna);
-        }
-    }
+		if (casillas[fila][columna].getMinasAdyacentes() == 0) {
+			revelarAdyacentes(fila, columna);
+		}
+	}
 
-    private void revelarAdyacentes(int fila, int columna) {
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                int nuevaFila = fila + x;
-                int nuevaColumna = columna + y;
-                if (dentroLimites(nuevaFila, nuevaColumna) && !casillas[nuevaFila][nuevaColumna].estaDescubierta()) {
-                    try {
-                        descubrir(convertirCoordenada(nuevaFila, nuevaColumna));
-                    } catch (ExcepcionJuego e) {
-                        // Ignorar excepciones durante la revelación automática
-                    }
-                }
-            }
-        }
-    }
+	private void revelarAdyacentes(int fila, int columna) {
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				int nuevaFila = fila + x;
+				int nuevaColumna = columna + y;
+				if (dentroLimites(nuevaFila, nuevaColumna) && !casillas[nuevaFila][nuevaColumna].estaDescubierta()) {
+					try {
+						descubrir(convertirCoordenada(nuevaFila, nuevaColumna));
+					} catch (ExcepcionJuego e) {
+						// Ignorar excepciones durante la revelación automática
+					}
+				}
+			}
+		}
+	}
 
-    public void marcar(String coordenada) throws ExcepcionJuego {
-        int[] indices = convertirCoordenada(coordenada);
-        int fila = indices[0];
-        int columna = indices[1];
+	public void marcar(String coordenada) throws ExcepcionJuego {
+		int[] indices = convertirCoordenada(coordenada);
+		int fila = indices[0];
+		int columna = indices[1];
 
-        if (casillas[fila][columna].estaDescubierta()) {
-            throw new CasillaYaDescubiertaException();
-        }
+		if (casillas[fila][columna].estaDescubierta()) {
+			throw new CasillaYaDescubiertaException();
+		}
 
-        casillas[fila][columna].marcar();
-    }
+		casillas[fila][columna].marcar();
+	}
 
-    public boolean victoria() {
-        return casillasSeguras == 0;
-    }
+	public boolean victoria() {
+		return casillasSeguras == 0;
+	}
 
-    public int getFilas() {
-        return filas;
-    }
+	// Getters y Setters
+	public int getFilas() {
+		return filas;
+	}
 
-    public int getColumnas() {
-        return columnas;
-    }
+	public int getColumnas() {
+		return columnas;
+	}
 
-    public Casilla getCasilla(int fila, int columna) {
-        return casillas[fila][columna];
-    }
+	public Casilla getCasilla(int fila, int columna) {
+		return casillas[fila][columna];
+	}
 
-    private int[] convertirCoordenada(String coordenada) throws ExcepcionJuego {
-        if (coordenada.length() < 2) {
-            throw new CoordenadaInvalidaException();
-        }
+	private int[] convertirCoordenada(String coordenada) throws ExcepcionJuego {
+		if (coordenada.length() < 2) {
+			throw new CoordenadaInvalidaException();
+		}
 
-        char filaChar = coordenada.charAt(0);
-        int columna;
-        try {
-            columna = Integer.parseInt(coordenada.substring(1)) - 1;
-        } catch (NumberFormatException e) {
-            throw new CoordenadaInvalidaException();
-        }
+		char filaChar = coordenada.charAt(0);
+		int columna;
+		try {
+			columna = Integer.parseInt(coordenada.substring(1)) - 1;
+		} catch (NumberFormatException e) {
+			throw new CoordenadaInvalidaException();
+		}
 
-        int fila = filaChar - 'A';
+		int fila = filaChar - 'A';
 
-        if (!dentroLimites(fila, columna)) {
-            throw new CoordenadaInvalidaException();
-        }
+		if (!dentroLimites(fila, columna)) {
+			throw new CoordenadaInvalidaException();
+		}
 
-        return new int[]{fila, columna};
-    }
+		return new int[] { fila, columna };
+	}
 
-    private String convertirCoordenada(int fila, int columna) {
-        return (char) ('A' + fila) + "" + (columna + 1);
-    }
+	private String convertirCoordenada(int fila, int columna) {
+		return (char) ('A' + fila) + "" + (columna + 1);
+	}
 }
